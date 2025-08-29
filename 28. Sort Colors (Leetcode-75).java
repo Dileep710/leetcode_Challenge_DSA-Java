0e43ccf421094512fd6b986d7a446891e8840ac5
Dileep Prasanth N Y <dileepprasanth710@gmail.com>
2025-08-29 14:19:17 +0530

    28. Sort Colors (Leetcode-75).java

---
+++ 28. Sort Colors (Leetcode-75).java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        Solution obj = new Solution();
+        obj.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
+class Solution
+{
+    public void sortColors(int[] nums)
+    {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+        while(mid <= high)
+        {
+            if(nums[mid] == 0)
+            {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }else if(nums[mid] == 1){
+                mid++;
+            }else
+            {
+                swap(nums, high, mid);
+                high--;
+            }
+        }
+    }
+    public void swap(int[] nums,int i,int j)
+    {
+        int k = nums[i];
+        nums[i] = nums[j];
+        nums[j] = k;
+    }
+}
